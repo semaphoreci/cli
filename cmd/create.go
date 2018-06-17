b22a0a4a54be6d21bfb3a90e2691e2e80083e8d1
Igor Šarčević <igor@renderedtext.com>
2018-06-17 16:17:48 +0200

    Load file based on flag

--- cmd/create.go
+++ cmd/create.go
@@ -2,21 +2,30 @@ package cmd
 
 import (
 	"fmt"
+  "io/ioutil"
 
 	"github.com/spf13/cobra"
+  // "gopkg.in/yaml.v2"
 )
 
 var createCmd = &cobra.Command{
 	Use:   "create",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Create a resource from a file.",
+	Long: ``,
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("")
+    path, _ := cmd.Flags().GetString("file")
+
+    fmt.Printf("%+v\n", path)
+
+    dat, err := ioutil.ReadFile(path)
+
+    if err != nil {
+      fmt.Printf("%+v\n", err)
+      return
+    }
+
+    fmt.Print(string(dat))
 	},
 }
 
