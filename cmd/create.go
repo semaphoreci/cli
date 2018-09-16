d78c91f4b6118b18c77b2571d1f1663651034f68
Igor Šarčević <igor@renderedtext.com>
2018-09-16 21:30:27 +0000

    Full create cmd

--- cmd/create.go
+++ cmd/create.go
@@ -1,7 +1,15 @@
 package cmd
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
 	cmd_create "github.com/semaphoreci/cli/cmd/create"
+	"github.com/semaphoreci/cli/cmd/utils"
+
+	client "github.com/semaphoreci/cli/api/client"
+	models "github.com/semaphoreci/cli/api/models"
 
 	"github.com/spf13/cobra"
 )
@@ -12,7 +20,62 @@ var createCmd = &cobra.Command{
 	Long:  ``,
 
 	Run: func(cmd *cobra.Command, args []string) {
-		RunCreate(cmd, args)
+		path, err := cmd.Flags().GetString("file")
+
+		utils.CheckWithMessage(err, "Path not provided")
+
+		data, err := ioutil.ReadFile(path)
+
+		utils.CheckWithMessage(err, "Failed to read from resource file.")
+
+		resource, err := parse_yaml_to_map(data)
+
+		utils.CheckWithMessage(err, "Failed to parse resource file.")
+
+		apiVersion := resource["apiVersion"].(string)
+		kind := resource["kind"].(string)
+
+		switch kind {
+		case "Project":
+			project, err := models.NewProjectV1AlphaFromYaml(data)
+
+			utils.Check(err)
+
+			c := client.NewProjectV1AlphaApi()
+
+			_, err = c.CreateProject(project)
+
+			utils.Check(err)
+
+			fmt.Printf("Project %s created.", project.Metadata.Name)
+		case "Secret":
+			secret, err := models.NewSecretV1BetaFromYaml(data)
+
+			utils.Check(err)
+
+			c := client.NewSecretV1BetaApi()
+
+			_, err = c.CreateSecret(secret)
+
+			utils.Check(err)
+
+			fmt.Printf("Secret %s created.", secret.Metadata.Name)
+		case "Dashboard":
+			dash, err := models.NewDashboardV1AlphaFromYaml(data)
+
+			utils.Check(err)
+
+			c := client.NewDashboardV1AlphaApi()
+
+			_, err = c.CreateDashboard(dash)
+
+			utils.Check(err)
+
+			fmt.Printf("Dashboard %s created.", dash.Metadata.Name)
+		default:
+			fmt.Fprintf(os.Stderr, "Unknow resource kind '%s'", kind)
+			os.Exit(1)
+		}
 	},
 }
 
@@ -23,28 +86,4 @@ func init() {
 
 	desc := "Filename, directory, or URL to files to use to create the resource"
 	createCmd.Flags().StringP("file", "f", "", desc)
-}
-
-func RunCreate(cmd *cobra.Command, args []string) {
-	// path, err := cmd.Flags().GetString("file")
-
-	// utils.CheckWithMessage(err, "Path not provided")
-
-	// data, err := ioutil.ReadFile(path)
-
-	// utils.CheckWithMessage(err, "Failed to read from resource file.")
-
-	// resource, err := parse_yaml_to_map(data)
-
-	// utils.CheckWithMessage(err, "Failed to parse resource file.")
-
-	// apiVersion := resource["apiVersion"].(string)
-	// kind := resource["kind"].(string)
-
-	// params := handler.CreateParams{ApiVersion: apiVersion, Resource: data}
-	// handler, err := handler.FindHandler(kind)
-
-	// utils.Check(err)
-
-	// handler.Create(params)
 }
