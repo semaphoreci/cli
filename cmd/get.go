a06761310e1776260d9d2a532c85f635cd412826
Igor Šarčević <igor@renderedtext.com>
2018-09-17 08:40:36 +0000

    Tests for get and list

--- cmd/get.go
+++ cmd/get.go
@@ -1,8 +1,12 @@
 package cmd
 
 import (
-	cmd_get "github.com/semaphoreci/cli/cmd/get"
+	"fmt"
+	"os"
+	"text/tabwriter"
 
+	client "github.com/semaphoreci/cli/api/client"
+	"github.com/semaphoreci/cli/cmd/utils"
 	"github.com/spf13/cobra"
 )
 
@@ -13,10 +17,141 @@ var getCmd = &cobra.Command{
 	Args:  cobra.RangeArgs(1, 2),
 }
 
+var GetDashboardCmd = &cobra.Command{
+	Use:     "dashboards [name]",
+	Short:   "Get dashboards.",
+	Long:    ``,
+	Aliases: []string{"dashboard", "dash"},
+	Args:    cobra.RangeArgs(0, 1),
+
+	Run: func(cmd *cobra.Command, args []string) {
+		c := client.NewDashboardV1AlphaApi()
+
+		if len(args) == 0 {
+			dashList, err := c.ListDashboards()
+
+			utils.Check(err)
+
+			const padding = 3
+			w := tabwriter.NewWriter(os.Stdout, 0, 0, padding, ' ', 0)
+
+			fmt.Fprintln(w, "NAME\tAGE")
+
+			for _, d := range dashList.Dashboards {
+				updateTime, err := d.Metadata.UpdateTime.Int64()
+
+				utils.Check(err)
+
+				fmt.Fprintf(w, "%s\t%s\n", d.Metadata.Name, utils.RelativeAgeForHumans(updateTime))
+			}
+
+			w.Flush()
+		} else {
+			name := args[0]
+
+			dash, err := c.GetDashboard(name)
+
+			utils.Check(err)
+
+			y, err := dash.ToYaml()
+
+			utils.Check(err)
+
+			fmt.Printf("%s", y)
+		}
+	},
+}
+
+var GetSecretCmd = &cobra.Command{
+	Use:     "secrets [name]",
+	Short:   "Get secrets.",
+	Long:    ``,
+	Aliases: []string{"secret"},
+	Args:    cobra.RangeArgs(0, 1),
+
+	Run: func(cmd *cobra.Command, args []string) {
+		c := client.NewSecretV1BetaApi()
+
+		if len(args) == 0 {
+			secretList, err := c.ListSecrets()
+
+			utils.Check(err)
+
+			const padding = 3
+			w := tabwriter.NewWriter(os.Stdout, 0, 0, padding, ' ', 0)
+
+			fmt.Fprintln(w, "NAME\tAGE")
+
+			for _, s := range secretList.Secrets {
+				updateTime, err := s.Metadata.UpdateTime.Int64()
+
+				utils.Check(err)
+
+				fmt.Fprintf(w, "%s\t%s\n", s.Metadata.Name, utils.RelativeAgeForHumans(updateTime))
+			}
+
+			w.Flush()
+		} else {
+			name := args[0]
+
+			secret, err := c.GetSecret(name)
+
+			utils.Check(err)
+
+			y, err := secret.ToYaml()
+
+			utils.Check(err)
+
+			fmt.Printf("%s", y)
+		}
+	},
+}
+
+var GetProjectCmd = &cobra.Command{
+	Use:     "projects [name]",
+	Short:   "Get projects.",
+	Long:    ``,
+	Aliases: []string{"project", "prj"},
+	Args:    cobra.RangeArgs(0, 1),
+
+	Run: func(cmd *cobra.Command, args []string) {
+		c := client.NewProjectV1AlphaApi()
+
+		if len(args) == 0 {
+			projectList, err := c.ListProjects()
+
+			utils.Check(err)
+
+			const padding = 3
+			w := tabwriter.NewWriter(os.Stdout, 0, 0, padding, ' ', 0)
+
+			fmt.Fprintln(w, "NAME\tREPOSITORY")
+
+			for _, p := range projectList.Projects {
+				fmt.Fprintf(w, "%s\t%s\n", p.Metadata.Name, p.Spec.Repository.Url)
+			}
+
+			w.Flush()
+		} else {
+			name := args[0]
+
+			project, err := c.GetProject(name)
+
+			utils.Check(err)
+
+			y, err := project.ToYaml()
+
+			utils.Check(err)
+
+			fmt.Printf("%s", y)
+		}
+	},
+}
+
 func init() {
 	RootCmd.AddCommand(getCmd)
 
-	getCmd.AddCommand(cmd_get.GetDashboardCmd)
-	getCmd.AddCommand(cmd_get.GetSecretCmd)
-	getCmd.AddCommand(cmd_get.GetProjectCmd)
+	getCmd.AddCommand(GetDashboardCmd)
+	getCmd.AddCommand(GetSecretCmd)
+	getCmd.AddCommand(GetProjectCmd)
 }
---
+++ cmd/get_test.go
@@ -0,0 +1,292 @@
+package cmd
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	httpmock "gopkg.in/jarcoal/httpmock.v1"
+)
+
+func Test__ListProjects__Response200(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	received := false
+
+	httpmock.RegisterResponder("GET", "https://org.semaphoretext.xyz/api/v1alpha/projects",
+		func(req *http.Request) (*http.Response, error) {
+			received = true
+
+			p1 := `{
+				"spec": {
+					"repository": {
+						"url" : "git@github.com:shiroyasha/advent-of-code-2017.git"
+					}
+				},
+				"metadata": {
+					"name":"advent-of-code-2017",
+					"id":"8f100520-5ab9-469f-854a-87bae95f19b9"
+				},
+				"kind":"Project",
+				"apiVersion":"v1alpha"
+			}`
+
+			p2 := `{
+				"spec": {
+					"repository": {
+						"url" : "git@github.com:shiroyasha/test.git"
+					}
+				},
+				"metadata": {
+					"name":"test",
+					"id":"1f100520-5ab9-469f-854a-87bae95f19b9"
+				},
+				"kind":"Project",
+				"apiVersion":"v1alpha"
+			}`
+
+			projects := fmt.Sprintf("[%s,%s]", p1, p2)
+
+			return httpmock.NewStringResponse(200, projects), nil
+		},
+	)
+
+	RootCmd.SetArgs([]string{"get", "projects"})
+	RootCmd.Execute()
+
+	if received == false {
+		t.Error("Expected the API to receive GET projects")
+	}
+}
+
+func Test__GetProject__Response200(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	received := false
+
+	httpmock.RegisterResponder("GET", "https://org.semaphoretext.xyz/api/v1alpha/projects/advent",
+		func(req *http.Request) (*http.Response, error) {
+			received = true
+
+			p1 := `{
+				"spec": {
+					"repository": {
+						"url" : "git@github.com:shiroyasha/advent-of-code-2017.git"
+					}
+				},
+				"metadata": {
+					"name":"advent",
+					"id":"8f100520-5ab9-469f-854a-87bae95f19b9"
+				},
+				"kind":"Project",
+				"apiVersion":"v1alpha"
+			}`
+
+			return httpmock.NewStringResponse(200, p1), nil
+		},
+	)
+
+	RootCmd.SetArgs([]string{"get", "projects", "advent"})
+	RootCmd.Execute()
+
+	if received == false {
+		t.Error("Expected the API to receive GET projects/test-prj")
+	}
+}
+
+func Test__ListDashboards__Response200(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	received := false
+
+	httpmock.RegisterResponder("GET", "https://org.semaphoretext.xyz/api/v1alpha/dashboards",
+		func(req *http.Request) (*http.Response, error) {
+			received = true
+
+			d1 := `{
+				"metadata": {
+					"name":"my-work",
+					"id":"07e64c23-4bca-4f62-b76e-4a85aef24bbb",
+					"title":"My Work",
+					"create_time":"1536843668",
+					"update_time":"1536843668"
+				},
+				"spec": {
+					"widgets": [{
+						"name": "Workflows",
+						"type": "list",
+						"filters":{
+							"github_uid":"{{github_uid}}"
+						}
+					}]
+				}
+			}`
+
+			d2 := `{
+				"metadata":{
+					"name":"everyones-activity",
+					"id":"5aabd382-a2b6-46d1-abb5-06d383dace08",
+					"title":"Everyone’s Activity",
+					"create_time":"1536843668",
+					"update_time":"1536843668"
+				},
+				"spec":{
+					"widgets":[{
+						"name":"Workflows",
+						"type":"list"
+					}]
+				}
+			}`
+
+			dashboards := fmt.Sprintf(`{"dashboards":[%s,%s]}`, d1, d2)
+
+			return httpmock.NewStringResponse(200, dashboards), nil
+		},
+	)
+
+	RootCmd.SetArgs([]string{"get", "dashboards"})
+	RootCmd.Execute()
+
+	if received == false {
+		t.Error("Expected the API to receive GET dashboards")
+	}
+}
+
+func Test__GetDashboard__Response200(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	received := false
+
+	httpmock.RegisterResponder("GET", "https://org.semaphoretext.xyz/api/v1alpha/dashboards/my-work",
+		func(req *http.Request) (*http.Response, error) {
+			received = true
+
+			d1 := `{
+				"metadata": {
+					"name":"my-work",
+					"id":"07e64c23-4bca-4f62-b76e-4a85aef24bbb",
+					"title":"My Work",
+					"create_time":"1536843668",
+					"update_time":"1536843668"
+				},
+				"spec": {
+					"widgets": [{
+						"name": "Workflows",
+						"type": "list",
+						"filters":{
+							"github_uid":"{{github_uid}}"
+						}
+					}]
+				}
+			}`
+
+			return httpmock.NewStringResponse(200, d1), nil
+		},
+	)
+
+	RootCmd.SetArgs([]string{"get", "dashboards", "my-work"})
+	RootCmd.Execute()
+
+	if received == false {
+		t.Error("Expected the API to receive GET dashboards/my-work")
+	}
+}
+
+func Test__ListSecrets__Response200(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	received := false
+
+	httpmock.RegisterResponder("GET", "https://org.semaphoretext.xyz/api/v1beta/secrets",
+		func(req *http.Request) (*http.Response, error) {
+			received = true
+
+			s1 := `{
+				"metadata":{
+					"name":"aaaaaaa",
+					"id":"bb2ba294-d4b3-48bc-90a7-12dd56e9424b",
+					"create_time":"1536673464",
+					"update_time":"1536674946"
+				},
+				"data":{
+					"env_vars":[{
+						"name":"TEST",
+						"value":"AAAA"
+					}],
+					"files":[{
+						"path":"a.txt",
+						"content":"W1twYWNrYWdlXV0KbmFtZSA9ICJzcGlyYWwtbWVtb3J5Igp2ZXJzaW9uID0gIjAuMS4wIgoK"
+					}]
+				}
+			}`
+
+			s2 := `{
+				"metadata":{
+					"name":"aaa-bbb",
+					"id":"d4cdb2aa-e9c6-4077-bf27-721c3a7993a9",
+					"create_time":"1536673704",
+					"update_time":"1536673704"
+				},
+				"data":{
+				}
+			}`
+
+			secrets := fmt.Sprintf(`{"secrets":[%s,%s]}`, s1, s2)
+
+			return httpmock.NewStringResponse(200, secrets), nil
+		},
+	)
+
+	RootCmd.SetArgs([]string{"get", "secrets"})
+	RootCmd.Execute()
+
+	if received == false {
+		t.Error("Expected the API to receive GET secrets")
+	}
+}
+
+func Test__GetSecret__Response200(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	received := false
+
+	httpmock.RegisterResponder("GET", "https://org.semaphoretext.xyz/api/v1beta/secrets/aaaaaaa",
+		func(req *http.Request) (*http.Response, error) {
+			received = true
+
+			s1 := `{
+				"metadata":{
+					"name":"aaaaaaa",
+					"id":"bb2ba294-d4b3-48bc-90a7-12dd56e9424b",
+					"create_time":"1536673464",
+					"update_time":"1536674946"
+				},
+				"data":{
+					"env_vars":[{
+						"name":"TEST",
+						"value":"AAAA"
+					}],
+					"files":[{
+						"path":"a.txt",
+						"content":"W1twYWNrYWdlXV0KbmFtZSA9ICJzcGlyYWwtbWVtb3J5Igp2ZXJzaW9uID0gIjAuMS4wIgoK"
+					}]
+				}
+			}`
+
+			return httpmock.NewStringResponse(200, s1), nil
+		},
+	)
+
+	RootCmd.SetArgs([]string{"get", "secrets", "aaaaaaa"})
+	RootCmd.Execute()
+
+	if received == false {
+		t.Error("Expected the API to receive GET secrets/aaaaaaa")
+	}
+}
