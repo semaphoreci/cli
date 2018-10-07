043d1ea2c5bda250d3efce1a1da3b679406c3760
Igor Šarčević <igor@renderedtext.com>
2018-10-07 15:37:31 +0000

    Introduce debug job

--- cmd/debug.go
+++ cmd/debug.go
@@ -115,6 +115,93 @@ var debugProjectCmd = &cobra.Command{
 	},
 }
 
+var debugJobCmd = &cobra.Command{
+	Use:     "project [NAME]",
+	Short:   "Debug a project",
+	Long:    ``,
+	Aliases: []string{"prj", "projects"},
+	Args:    cobra.ExactArgs(1),
+
+	Run: func(cmd *cobra.Command, args []string) {
+		publicKey := viper.GetString("debug.PublicSshKey")
+
+		if publicKey == "" {
+			fmt.Println("[ERROR] Public SSH key for debugging is not configured.")
+			fmt.Println(unsetPublicSshKeyMsg)
+
+			os.Exit(1)
+		}
+
+		project_name := args[0]
+		pc := client.NewProjectV1AlphaApi()
+		project, err := pc.GetProject(project_name)
+
+		utils.Check(err)
+
+		jobName := fmt.Sprintf("Debug Session for %s", project_name)
+		job := models.NewJobV1Alpha(jobName)
+
+		job.Spec = &models.JobV1AlphaSpec{}
+		job.Spec.Agent.Machine.Type = "e2-standard-2"
+		job.Spec.Agent.Machine.OsImage = "ubuntu1804"
+		job.Spec.ProjectId = project.Metadata.Id
+
+		job.Spec.Commands = []string{
+			fmt.Sprintf("echo '%s' >> .ssh/authorized_keys", publicKey),
+			"sleep infinity",
+		}
+
+		c := client.NewJobsV1AlphaApi()
+
+		job, err = c.CreateJob(job)
+
+		utils.Check(err)
+
+		for {
+			time.Sleep(1000 * time.Millisecond)
+
+			job, err = c.GetJob(job.Metadata.Id)
+
+			utils.Check(err)
+
+			if job.Status.State == "FINISHED" {
+				fmt.Printf("Job %s has already finished.\n", job.Metadata.Id)
+				os.Exit(0)
+			}
+
+			if job.Status.State != "RUNNING" {
+				fmt.Printf("Waiting for Job %s to start.\n", job.Metadata.Id)
+				continue
+			}
+
+			break
+		}
+
+		ip := job.Status.Agent.Ip
+
+		var ssh_port int32
+		ssh_port = 0
+
+		for _, p := range job.Status.Agent.Ports {
+			if p.Name == "ssh" {
+				ssh_port = p.Number
+			}
+		}
+
+		if ip != "" && ssh_port != 0 {
+			time.Sleep(1000 * time.Millisecond)
+
+			err := utils.SshIntoAJob(ip, ssh_port, "semaphore")
+
+			utils.Check(err)
+		} else {
+			fmt.Printf("Job %s has no exposed SSH port.\n", job.Metadata.Id)
+			os.Exit(1)
+		}
+
+	},
+}
+
 var debugCmd = &cobra.Command{
 	Use:   "debug",
 	Short: "Debug a resource.",
@@ -123,6 +210,7 @@ var debugCmd = &cobra.Command{
 
 func init() {
 	debugCmd.AddCommand(debugProjectCmd)
+	debugCmd.AddCommand(debugJobCmd)
 
 	RootCmd.AddCommand(debugCmd)
 }
