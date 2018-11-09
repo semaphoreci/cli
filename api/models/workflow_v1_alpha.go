6cb0634f6f85e0e002e7ce53f0c32dd6d479fe35
Predrag Rakic <prakic@renderedtext.com>
2018-11-09 23:56:33 +0100

    display creation time in get wf

--- api/models/workflow_v1_alpha.go
+++ api/models/workflow_v1_alpha.go
@@ -4,4 +4,7 @@ type WorkflowV1Alpha struct {
 	Id           string `json:"wf_id,omitempty" yaml:"id,omitempty"`
 	InitialPplId string `json:"initial_ppl_id,omitempty" yaml:"initial_ppl_id,omitempty"`
 	BranchName   string `json:"branch_name,omitempty" yaml:"branch_name,omitempty"`
+	CreatedAt struct {
+		Seconds int64 `json:"seconds"`
+	} `json:"created_at"`
 }
