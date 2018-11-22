554c821cc5b3b47ce5aff9bee5fff72454e2a0b3
Igor Šarčević <igor@renderedtext.com>
2018-11-22 16:38:02 +0100

    Stop job

--- cmd/stop_test.go
+++ cmd/stop_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"testing"
 
+	assert "github.com/stretchr/testify/assert"
 	httpmock "gopkg.in/jarcoal/httpmock.v1"
 )
 
@@ -32,4 +33,24 @@ func Test__StopPipeline__Response200(t *testing.T) {
 	if received != expected {
 		t.Errorf("Expected the API to receive PATCH pipelines with: %s, got: %s", expected, received)
 	}
+}
+
+func Test__StopJob__Response200(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	received := false
+
+	httpmock.RegisterResponder("PATCH", "https://org.semaphoretext.xyz/api/v1alpha/jobs/494b76aa-f3f0-4ecf-b5ef-c389591a01be/stop",
+		func(req *http.Request) (*http.Response, error) {
+			received = true
+
+			return httpmock.NewStringResponse(200, "message"), nil
+		},
+	)
+
+	RootCmd.SetArgs([]string{"stop", "job", "494b76aa-f3f0-4ecf-b5ef-c389591a01be"})
+	RootCmd.Execute()
+
+	assert.Equal(t, received, true)
 }
