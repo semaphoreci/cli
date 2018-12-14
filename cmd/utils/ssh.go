c9f5b5b02fa31fa92d7abace70860858f987329f
Igor Šarčević <igor@renderedtext.com>
2018-12-14 20:39:34 +0100

    Better UX for debug sessions

--- cmd/utils/ssh.go
+++ cmd/utils/ssh.go
@@ -38,8 +38,10 @@ func GetPublicSshKey() (string, error) {
 	return publicKey, nil
 }
 
-func WaitForStartAndSsh(c *client.JobsApiV1AlphaApi, job *models.JobV1Alpha) {
+func WaitForStartAndSsh(c *client.JobsApiV1AlphaApi, job *models.JobV1Alpha, sshIntroMessage string) {
 	var err error
+
+	fmt.Printf("* Waiting for debug session to boot up .")
 
 	for {
 		time.Sleep(1000 * time.Millisecond)
@@ -49,17 +51,19 @@ func WaitForStartAndSsh(c *client.JobsApiV1AlphaApi, job *models.JobV1Alpha) {
 		Check(err)
 
 		if job.Status.State == "FINISHED" {
-			fmt.Printf("Job %s has already finished.\n", job.Metadata.Id)
-			os.Exit(0)
+			fmt.Printf("\nError while starting SSH session Job. Debug Job '%s' is in finished state.\n", job.Metadata.Id)
+			os.Exit(1)
 		}
 
 		if job.Status.State != "RUNNING" {
-			fmt.Printf("Waiting for Job %s to start.\n", job.Metadata.Id)
+			fmt.Print(".")
 			continue
 		}
 
 		break
 	}
+
+	fmt.Printf("\n")
 
 	ip := job.Status.Agent.Ip
 
@@ -72,15 +76,27 @@ func WaitForStartAndSsh(c *client.JobsApiV1AlphaApi, job *models.JobV1Alpha) {
 		}
 	}
 
-	if ip != "" && ssh_port != 0 {
-		time.Sleep(2000 * time.Millisecond)
+	if ip == "" || ssh_port == 0 {
+		fmt.Printf("Job %s has no exposed SSH ports.\n", job.Metadata.Id)
+		os.Exit(1)
+	}
 
-		err := SshIntoAJob(ip, ssh_port, "semaphore")
+	fmt.Print(sshIntroMessage)
+	fmt.Print("\n")
 
+	time.Sleep(2000 * time.Millisecond)
+
+	err = SshIntoAJob(ip, ssh_port, "semaphore")
+
+	fmt.Printf("\n")
+	fmt.Printf("* Stopping debug session ..\n")
+
+	err = c.StopJob(job.Metadata.Id)
+
+	if err != nil {
 		Check(err)
 	} else {
-		fmt.Printf("Job %s has no exposed SSH port.\n", job.Metadata.Id)
-		os.Exit(1)
+		fmt.Printf("* Session stopped\n")
 	}
 }
 
@@ -92,13 +108,13 @@ func SshIntoAJob(ip string, port int32, username string) error {
 	}
 
 	portFlag := fmt.Sprintf("-p%d", port)
+	interactive := "-t"
+	supressMotd := "-q"
 	noStrictFlag := "-oStrictHostKeyChecking=no"
 	timeoutFlag := "-oConnectTimeout=10"
 	userAndIp := fmt.Sprintf("%s@%s", username, ip)
 
-	fmt.Printf("%s %s %s %s %s\n", ssh_path, portFlag, timeoutFlag, noStrictFlag, userAndIp)
-
-	ssh_cmd := exec.Command(ssh_path, portFlag, timeoutFlag, noStrictFlag, userAndIp)
+	ssh_cmd := exec.Command(ssh_path, interactive, supressMotd, portFlag, timeoutFlag, noStrictFlag, userAndIp, "bash --login")
 
 	ssh_cmd.Stdin = os.Stdin
 	ssh_cmd.Stdout = os.Stdout
