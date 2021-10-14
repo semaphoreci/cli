ae2a17658323feb68fa03dd8b0b2bfa2418eafc2
Raphael Lopes <raphaeljlps@icloud.com>
2021-10-14 10:44:01 -0300

    bugfix: adds check to prevent index out of range runtime error
    Adds a check to prevent index out of range error while trying to access
    index 0 of an empty array. Also adds an error message that gives a clue
    on what may be the issue.
    
    Signed-off-by: Raphael Lopes <raphaeljlps@gmail.com>

--- cmd/workflows/describe.go
+++ cmd/workflows/describe.go
@@ -1,13 +1,14 @@
 package workflows
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
-	"time"
 	"os"
 	"text/tabwriter"
-	"encoding/json"
+	"time"
 
-	client "github.com/semaphoreci/cli/api/client"
+	"github.com/semaphoreci/cli/api/client"
 	"github.com/semaphoreci/cli/api/models"
 	"github.com/semaphoreci/cli/cmd/utils"
 )
@@ -22,11 +23,16 @@ func Describe(projectID, wfID string) {
 
 func prettyPrintPipelineList(jsonList []byte) {
   j := models.PipelinesListV1Alpha{}
-	err := json.Unmarshal(jsonList, &j)
+  err := json.Unmarshal(jsonList, &j)
   utils.Check(err)
 
   const padding = 3
   w := tabwriter.NewWriter(os.Stdout, 0, 0, padding, ' ', 0)
+  defer w.Flush()
+
+  if len(j) == 0 {
+	utils.Check(errors.New("failed with empty response, check if the workflow id is correct"))
+  }
 
   fmt.Fprintf(w, "Label: %s\n\n", j[0].Label)
   fmt.Fprintln(w, "PIPELINE ID\tPIPELINE NAME\tCREATION TIME\tSTATE")
@@ -35,6 +41,4 @@ func prettyPrintPipelineList(jsonList []byte) {
     createdAt := time.Unix(p.CreatedAt.Seconds, 0).Format("2006-01-02 15:04:05")
     fmt.Fprintf(w, "%s\t%s\t%s\t%s\n", p.Id, p.Name, createdAt, p.State)
   }
-
-  w.Flush()
 }
