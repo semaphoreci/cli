43a49190f737e1280bbebffe8984a60fb360721a
Miguel Paolino <mpaolino@gmail.com>
2022-03-18 14:10:11 -0300

    Adds BaseClient's ListWithParams test

--- api/client/base_client_test.go
+++ api/client/base_client_test.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"net/http"
+	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -45,6 +46,95 @@ func Test__Get__InvalidURL(t *testing.T) {
 	client := NewBaseClient("MYTOKEN", "org.semaphoretext.xyz", "testapi")
 
 	response, statusCode, err := client.Get("kind", "^-^")
+
+	assert.Error(t, err)
+	assert.Equal(t, 0, statusCode)
+	assert.Equal(t, []byte(""), response)
+}
+
+func Test__List__Response200(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	received := false
+
+	userAgent := "Test-User-Agent"
+	UserAgent = userAgent
+
+	httpmock.RegisterResponder("GET", "https://org.semaphoretext.xyz/api/testapi/kind",
+		func(req *http.Request) (*http.Response, error) {
+			received = true
+			assert.Equal(t, "Token MYTOKEN", req.Header.Get("Authorization"))
+			assert.Equal(t, "application/json", req.Header.Get("Content-Type"))
+			assert.Equal(t, userAgent, req.Header.Get("User-Agent"))
+
+			return httpmock.NewStringResponse(200, ""), nil
+		},
+	)
+
+	client := NewBaseClient("MYTOKEN", "org.semaphoretext.xyz", "testapi")
+
+	response, statusCode, err := client.List("kind")
+
+	assert.NoError(t, err)
+	assert.Equal(t, 200, statusCode)
+	assert.Equal(t, []byte(""), response)
+
+	if received == false {
+		t.Error("Expected the API to receive GET request")
+	}
+}
+
+func Test__List__InvalidURL(t *testing.T) {
+	client := NewBaseClient("MYTOKEN", "org.semaphoretext.xyz", "testapi")
+
+	response, statusCode, err := client.List("^-^")
+
+	assert.Error(t, err)
+	assert.Equal(t, 0, statusCode)
+	assert.Equal(t, []byte(""), response)
+}
+
+func Test__ListWithParams__Response200(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	received := false
+
+	userAgent := "Test-User-Agent"
+	UserAgent = userAgent
+
+	values := url.Values{}
+	values.Add("some", "value")
+
+	httpmock.RegisterResponder("GET", "https://org.semaphoretext.xyz/api/testapi/kind",
+		func(req *http.Request) (*http.Response, error) {
+			received = true
+			assert.Equal(t, "Token MYTOKEN", req.Header.Get("Authorization"))
+			assert.Equal(t, "application/json", req.Header.Get("Content-Type"))
+			assert.Equal(t, userAgent, req.Header.Get("User-Agent"))
+			assert.Equal(t, "value", req.URL.Query().Get("some"))
+			return httpmock.NewStringResponse(200, ""), nil
+		},
+	)
+
+	client := NewBaseClient("MYTOKEN", "org.semaphoretext.xyz", "testapi")
+
+	response, statusCode, err := client.ListWithParams("kind", values)
+
+	assert.NoError(t, err)
+	assert.Equal(t, 200, statusCode)
+	assert.Equal(t, []byte(""), response)
+
+	if received == false {
+		t.Error("Expected the API to receive GET request")
+	}
+}
+
+func Test__ListWithParams__InvalidURL(t *testing.T) {
+	client := NewBaseClient("MYTOKEN", "org.semaphoretext.xyz", "testapi")
+
+	response, statusCode, err := client.ListWithParams("^-^", url.Values{})
 
 	assert.Error(t, err)
 	assert.Equal(t, 0, statusCode)
