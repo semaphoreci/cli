1a1277271154b4c51ee811b8044de2a94292cfae
Miguel Paolino <mpaolino@gmail.com>
2022-03-18 15:31:39 -0300

    Added Delete and PostHeaders tests for BaseClient

--- api/client/base_client_test.go
+++ api/client/base_client_test.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"io"
 	"net/http"
 	"net/url"
 	"testing"
@@ -38,7 +39,7 @@ func Test__Get__Response200(t *testing.T) {
 	assert.Equal(t, []byte(""), response)
 
 	if received == false {
-		t.Error("Expected the API to receive GET request")
+		t.Error("Expected the API to receive a GET request")
 	}
 }
 
@@ -81,7 +82,7 @@ func Test__List__Response200(t *testing.T) {
 	assert.Equal(t, []byte(""), response)
 
 	if received == false {
-		t.Error("Expected the API to receive GET request")
+		t.Error("Expected the API to receive a GET request")
 	}
 }
 
@@ -127,7 +128,7 @@ func Test__ListWithParams__Response200(t *testing.T) {
 	assert.Equal(t, []byte(""), response)
 
 	if received == false {
-		t.Error("Expected the API to receive GET request")
+		t.Error("Expected the API to receive a GET request")
 	}
 }
 
@@ -135,6 +136,99 @@ func Test__ListWithParams__InvalidURL(t *testing.T) {
 	client := NewBaseClient("MYTOKEN", "org.semaphoretext.xyz", "testapi")
 
 	response, statusCode, err := client.ListWithParams("^-^", url.Values{})
+
+	assert.Error(t, err)
+	assert.Equal(t, 0, statusCode)
+	assert.Equal(t, []byte(""), response)
+}
+
+func Test__Delete__Response200(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	received := false
+
+	userAgent := "Test-User-Agent"
+	UserAgent = userAgent
+
+	values := url.Values{}
+	values.Add("some", "value")
+
+	httpmock.RegisterResponder("DELETE", "https://org.semaphoretext.xyz/api/testapi/kind/resource",
+		func(req *http.Request) (*http.Response, error) {
+			received = true
+			assert.Equal(t, "Token MYTOKEN", req.Header.Get("Authorization"))
+			assert.Equal(t, "application/json", req.Header.Get("Content-Type"))
+			assert.Equal(t, userAgent, req.Header.Get("User-Agent"))
+			return httpmock.NewStringResponse(200, ""), nil
+		},
+	)
+
+	client := NewBaseClient("MYTOKEN", "org.semaphoretext.xyz", "testapi")
+
+	response, statusCode, err := client.Delete("kind", "resource")
+
+	assert.NoError(t, err)
+	assert.Equal(t, 200, statusCode)
+	assert.Equal(t, []byte(""), response)
+
+	if received == false {
+		t.Error("Expected the API to receive a DELETE request")
+	}
+}
+
+func Test__Delete__InvalidURL(t *testing.T) {
+	client := NewBaseClient("MYTOKEN", "org.semaphoretext.xyz", "testapi")
+
+	response, statusCode, err := client.Delete("kind", "^-^")
+
+	assert.Error(t, err)
+	assert.Equal(t, 0, statusCode)
+	assert.Equal(t, []byte(""), response)
+}
+
+func Test__PostHeaders__Response201(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	received := false
+
+	userAgent := "Test-User-Agent"
+	UserAgent = userAgent
+	headers := map[string]string{"key": "value"}
+	client := NewBaseClient("MYTOKEN", "org.semaphoretext.xyz", "testapi")
+	resource := []byte("resource")
+
+	httpmock.RegisterResponder("POST", "https://org.semaphoretext.xyz/api/testapi/kind",
+		func(req *http.Request) (*http.Response, error) {
+			received = true
+			assert.Equal(t, "Token MYTOKEN", req.Header.Get("Authorization"))
+			assert.Equal(t, "application/json", req.Header.Get("Content-Type"))
+			assert.Equal(t, userAgent, req.Header.Get("User-Agent"))
+			assert.Equal(t, "value", req.Header.Get("key"))
+
+			body, err := io.ReadAll(req.Body)
+			assert.NoError(t, err)
+			assert.Equal(t, "resource", string(body))
+			return httpmock.NewStringResponse(201, ""), nil
+		},
+	)
+
+	response, statusCode, err := client.PostHeaders("kind", resource, headers)
+
+	assert.NoError(t, err)
+	assert.Equal(t, 201, statusCode)
+	assert.Equal(t, []byte(""), response)
+
+	if received == false {
+		t.Error("Expected the API to receive a POST request")
+	}
+}
+
+func Test__PostHeaders__InvalidURL(t *testing.T) {
+	client := NewBaseClient("MYTOKEN", "org.semaphoretext.xyz", "testapi")
+
+	response, statusCode, err := client.PostHeaders("^-^", []byte(""), map[string]string{})
 
 	assert.Error(t, err)
 	assert.Equal(t, 0, statusCode)
