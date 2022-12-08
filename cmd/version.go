dab8cc9ae950e6ba9e71a50cdba218d0cfda286c
Amir Hasanbasic <ahasanbasic@renderedtext.com>
2022-12-08 22:30:53 +0100

    Add outdated version warning

--- cmd/version.go
+++ cmd/version.go
@@ -2,7 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"runtime"
 
+	"github.com/hamir-suspect/go-latest"
+	"github.com/hashicorp/go-version"
 	"github.com/spf13/cobra"
 )
 
@@ -26,4 +29,37 @@ var versionCmd = &cobra.Command{
 
 func init() {
 	RootCmd.AddCommand(versionCmd)
+	if runtime.GOOS == "windows" {
+		Reset  = ""
+		Yellow = ""
+		Green = ""
+	}
+}
+
+var Reset  = "\033[0m"
+var Green= "\033[32m"
+var Yellow = "\033[33m"
+var outdatedVersionMsgTemplate = "%swarn: You are using outdated version of cli: %s newest version is: %s\nInstall the new version using this snipet: %s%s%s\n"
+var InstallScript = "curl https://storage.googleapis.com/sem-cli-releases/get.sh | bash"
+
+func CheckNewerVersion() {
+	githubTag := latest.GithubTag{
+		Owner: "semaphoreci",
+		Repository: "cli",
+	}
+	
+	// ignore version check if using dev version
+	_, err := version.NewSemver(ReleaseVersion)
+	if err != nil {
+		return
+	}
+	
+	res, err := latest.Check(&githubTag, "v"+ReleaseVersion)
+	if err!= nil {
+		fmt.Println("error checking the version: ", err.Error())
+		return
+	}
+	if res.Outdated {
+		fmt.Printf(outdatedVersionMsgTemplate, Yellow, "v"+ReleaseVersion, res.Current, Green, InstallScript, Reset)
+	}
 }
--- go.mod
+++ go.mod
@@ -5,7 +5,11 @@ go 1.16
 require (
 	github.com/BurntSushi/toml v0.3.0 // indirect
 	github.com/ghodss/yaml v1.0.0
+	github.com/google/go-github v17.0.0+incompatible // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/google/uuid v1.0.0
+	github.com/hamir-suspect/go-latest v0.0.0-20170313132115-e3007ae9052e
+	github.com/hashicorp/go-version v1.6.0
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/mitchellh/go-homedir v1.0.0
 	github.com/onsi/gomega v1.4.2 // indirect
@@ -14,6 +18,7 @@ require (
 	github.com/spf13/viper v1.2.0
 	github.com/stretchr/testify v1.2.2
 	github.com/tcnksm/go-gitconfig v0.1.2
+	golang.org/x/net v0.4.0 // indirect
 	gopkg.in/jarcoal/httpmock.v1 v1.0.0-20180719183105-8007e27cdb32
 	gopkg.in/yaml.v2 v2.2.1
 )
